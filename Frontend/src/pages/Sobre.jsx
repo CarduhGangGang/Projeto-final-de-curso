318f4225205d1adb4b5f8249c74c616970ecf153
CarduhGangGang <dudajunkysk8@gmail.com>
2025-06-21 13:53:36 +0100

    whatt

--- Frontend/src/pages/Sobre.jsx
+++ Frontend/src/pages/Sobre.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Hero from "../components/Sobre/Hero";
-import NossaVisao from "../components/Sobre/NossaVisao"; 
+import NossaHistoria from "../components/Sobre/NossaHistoria";
+import NossaVisao from "../components/Sobre/NossaVisao";
 import Oquefazemos from "../components/Sobre/Oquefazemos";
 import Connects from "../components/Sobre/Connects";
 import Footer from "../components/Footer";
-import NossaHistoria from "../components/Sobre/NossaHistoria";
-
 
 const Sobre = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">
-      <Hero />
-      <NossaHistoria />
-      <NossaVisao /> 
-      <Oquefazemos />
-      <Connects />
+        <Hero />
+        <NossaHistoria />
+        <NossaVisao />
+        <Oquefazemos />
+        <Connects />
       </main>
       <Footer />
     </div>
